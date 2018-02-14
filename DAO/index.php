46efed3434958ca80b521eb4d8028aabe37e28ea
Felipe Tavares <felipe.tavares@outlook.com>
2018-02-13 22:57:05 -0200

    Métodos de listas

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,22 @@
 
 require_once("Config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(5);
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo id
+//$search = Usuario::search("o");
+//echo json_encode($search);
+
+//Carrega uma lita usandno o login e senha
+$usuario = new Usuario();
+$usuario->login("admin","admin");
+echo $usuario;
 
 ?>
